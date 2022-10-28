54570db4f73d5cc75db692eb1abeecd2c17cff17
Abel Aron Amieva <abelamieva@gmail.com>
2022-10-28 19:47:58 -0300

    Primer commit

---
+++ api.js
@@ -0,0 +1,185 @@
+const path = require('path');
+const MTProto = require('@mtproto/core');
+const { sleep } = require('@mtproto/core/src/utils/common');
+const fs = require('fs');
+
+class API {
+    constructor() {
+        this.mtproto = new MTProto({
+            api_id: process.env.API_ID,
+            api_hash: process.env.API_HASH,
+
+            storageOptions: {
+                path: path.resolve(__dirname, './data/1.json'),
+            },
+        });
+
+        this.messagesToReply = [];
+        this.messages = [];
+        this.subscribeToUpdates();
+    }
+
+    async call(method, params, options = {}) {
+        try {
+            const result = await this.mtproto.call(method, params, options);
+
+            return result;
+        } catch (error) {
+            console.log(`${method} error:`, error);
+
+            const { error_code, error_message } = error;
+
+            if (error_code === 420) {
+                const seconds = Number(error_message.split('FLOOD_WAIT_')[1]);
+                const ms = seconds * 1000;
+
+                await sleep(ms);
+
+                return this.call(method, params, options);
+            }
+
+            if (error_code === 303) {
+                const [type, dcIdAsString] = error_message.split('_MIGRATE_');
+
+                const dcId = Number(dcIdAsString);
+
+                // If auth.sendCode call on incorrect DC need change default DC, because
+                // call auth.signIn on incorrect DC return PHONE_CODE_EXPIRED error
+                if (type === 'PHONE') {
+                    await this.mtproto.setDefaultDc(dcId);
+                } else {
+                    Object.assign(options, { dcId });
+                }
+
+                return this.call(method, params, options);
+            }
+
+            return Promise.reject(error);
+        }
+    }
+
+    async getUser() {
+        try {
+            const user = await this.call('users.getFullUser', {
+                id: {
+                    _: 'inputUserSelf',
+                },
+            });
+
+            return user;
+        } catch (error) {
+            return null;
+        }
+    }
+
+    sendCode(phone) {
+        return this.call('auth.sendCode', {
+            phone_number: phone,
+            settings: {
+                _: 'codeSettings',
+            },
+        });
+    }
+
+    signIn({ code, phone, phone_code_hash }) {
+        return this.call('auth.signIn', {
+            phone_code: code,
+            phone_number: phone,
+            phone_code_hash: phone_code_hash,
+        });
+    }
+
+    signUp({ phone, phone_code_hash }) {
+        return this.call('auth.signUp', {
+            phone_number: phone,
+            phone_code_hash: phone_code_hash,
+            first_name: 'MTProto',
+            last_name: 'Core',
+        });
+    }
+
+    getPassword() {
+        return this.call('account.getPassword');
+    }
+
+    checkPassword({ srp_id, A, M1 }) {
+        return this.call('auth.checkPassword', {
+            password: {
+                _: 'inputCheckPasswordSRP',
+                srp_id,
+                A,
+                M1,
+            },
+        });
+    }
+
+    async getHistory(username) {
+        console.log('Fetch history messages of: ', username)
+        const resolveGroup = await this.call('contacts.resolveUsername', {
+            username: typeof username == 'string' ? username.replace('@', '') : username,
+        })
+
+
+        const hash = resolveGroup.chats[0].access_hash;
+        const id = resolveGroup.chats[0].id;
+
+        return (await this.call('messages.getHistory', {
+            peer: {
+                _: 'inputPeerChannel',
+                channel_id: id,
+                access_hash: hash
+            },
+            max_id: 0,
+            offset: 0,
+            limit: 10
+        })).messages;
+    }
+
+    subscribeToUpdates() {
+
+        this.mtproto.updates.on('updates', (updateInfo) => {
+
+            console.log('Updated..')
+
+            if ((updateInfo.updates[0]._ == 'updateNewChannelMessage')) {
+
+                const data = `Canal: ${updateInfo.chats[0].title}\nEl id del canal es :${updateInfo.chats[0].id}\nEl hash del canal es: ${updateInfo.chats[0].access_hash}\nEL id del usuario que escribio es: ${updateInfo.updates[0].message.from_id.user_id}`;
+
+                fs.writeFileSync(`info/${updateInfo.chats[0].title}.txt`, data);
+
+            }
+
+            updateInfo.updates.forEach(update => {
+                // this.messages.push(update.message)
+                if ((update._ == 'updateNewChannelMessage') && (update.message.from_id.user_id == process.env.C_USER_ID) && (update.message.peer_id.channel_id == process.env.C_CHANNEL_ID)) {
+                    this.messagesToReply.push(update.message.message)
+                    this.sendMessageToChannel();
+                };
+            })
+        })
+    }
+
+    async sendMessageToChannel() {
+        try {
+
+            const resp = await this.call('messages.sendMessage', {
+                peer: {
+                    _: 'inputPeerChannel',
+                    channel_id: process.env.R_CHANNEL_ID,
+                    access_hash: process.env.R_CHANNEL_HASH // hash del canal de pruebas
+                },
+                random_id: Math.ceil(Math.random() * 0xffffff) + Math.ceil(Math.random() * 0xffffff),
+                message: this.messagesToReply[0]
+            });
+            this.messagesToReply = []
+        } catch (error) {
+            console.log('error on sendMessageToChannel', error);
+        }
+
+    }
+
+}
+
+const api = new API();
+
+module.exports = api;
---
+++ index.js
@@ -0,0 +1,72 @@
+const input = require('input');
+const api = require("./api");
+
+
+const customInput = async ( message )=>{
+    return await  input.text(message);
+}
+
+
+const init = async () => {
+
+
+    const user = await api.getUser();
+    
+    
+    if (!user) {
+        
+        const phone = await customInput('Please, what is your phone number?' );
+        
+        const { phone_code_hash } = await api.sendCode(phone); // esta funcion dispara el envio del codigo
+
+        const code = await customInput('Whats is de phone code?'); // hay que ver si funciona
+
+
+        try {
+            const signInResult = await api.signIn({
+                code,
+                phone,
+                phone_code_hash,
+            });
+
+            if (signInResult._ === 'auth.authorizationSignUpRequired') {
+                await api.signUp({
+                    phone,
+                    phone_code_hash,
+                });
+            }
+        } catch (error) {
+            if (error.error_message !== 'SESSION_PASSWORD_NEEDED') {
+                console.log(`error:`, error);
+
+                return;
+            }
+
+            // 2FA
+
+            const password = await customInput('Please, what is your password?' );
+
+            const { srp_id, current_algo, srp_B } = await api.getPassword();
+            const { g, p, salt1, salt2 } = current_algo;
+
+            const { A, M1 } = await api.mtproto.crypto.getSRPParams({
+                g,
+                p,
+                salt1,
+                salt2,
+                gB: srp_B,
+                password,
+            });
+
+            const checkPasswordResult = await api.checkPassword({ srp_id, A, M1 });
+        }
+    }
+    console.clear();
+    console.log('Telegram initilized...')
+};
+
+
+init().then( resp => {
+})
+
+
