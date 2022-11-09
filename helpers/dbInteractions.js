8ae7c01519c78c294c751967c13ee6d6a1f66699
Abel Aron Amieva <abelamieva@gmail.com>
2022-11-09 16:14:48 -0300

    se creo base de datos para mensajes

--- helpers/dbInteractions.js
+++ helpers/dbInteractions.js
@@ -1,24 +1,42 @@
 const fs = require('fs');
 
-const archivo= `./db/dataBase.json`;
+const archivo = `./db/dataBase.json`;
+const archivoMensajes = `./db/mensajes.json`;
 
 
-const cargarDataBase = ( data )=>{
+const cargarDataBase = (data) => {
     fs.writeFileSync(archivo, JSON.stringify(data));
 };
 
-const leerDataBase = ()=>{
+const leerDataBase = () => {
 
-    
-    if(!fs.existsSync(archivo)) return null;
 
-    const info = fs.readFileSync( archivo, { encoding: 'utf-8'});
+    if (!fs.existsSync(archivo)) return null;
+
+    const info = fs.readFileSync(archivo, { encoding: 'utf-8' });
     return JSON.parse(info);
 
-    
-}
 
-module.exports={
+}
+const leerDataBaseMensajes = () => {
+
+
+    if (!fs.existsSync(archivoMensajes)) return null;
+
+    const info = fs.readFileSync(archivoMensajes, { encoding: 'utf-8' });
+    return JSON.parse(info);
+
+
+}
+const cargarDataBaseMensajes = (data) => {
+    
+    fs.writeFileSync(archivoMensajes, JSON.stringify(data));
+
+};
+
+module.exports = {
     cargarDataBase,
-    leerDataBase
+    leerDataBase,
+    leerDataBaseMensajes,
+    cargarDataBaseMensajes
 }
